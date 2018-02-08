4662f6fcf7771e194f638b9b08dbf9827d5b2e39
Marley Zelinger <marley.zelinger@gmail.com>
2018-02-07 22:29:41 -0500

    more solutions to run away button

--- Color_Randomizer/script.js
+++ Color_Randomizer/script.js
@@ -1,6 +1,5 @@
 var box = document.getElementById('color');
 box.addEventListener('click', function(){
-
 box.style.backgroundColor = 'rgb('+ Math.floor(Math.random()*255) + ',' + Math.floor(Math.random()*255) + ','+ Math.floor(Math.random()*255) + ')'
 
 
--- Runaway_Button_Lab/script.js
+++ Runaway_Button_Lab/script.js
@@ -1,7 +1,14 @@
 var button = document.getElementById('b');
+var down = 0;
 button.addEventListener('click', function(){
-  var left = Math.random()*95;
-  var top = Math.random()*95;
-  button.style.left = "" + left + "%";
-  button.style.top = "" + top + "%";
+  // moving down and diagonal
+  button.style.top = down + 50 + "px";
+  button.style.left = down + 50 + "px";
+  down = down + 50;
+
+  // random location
+  // var left = Math.random()*95;
+  // var top = Math.random()*95;
+  // button.style.left = "" + left + "%";
+  // button.style.top = "" + top + "%";
 })
